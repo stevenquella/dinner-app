1f066eab20cfdcc54c943f4bf1de203c4c477594
Steven Quella <stevenquella@gmail.com>
2022-02-08 19:03:29 -0500

    Testing authorized route.

--- dinner.app/src/lib/routes.ts
+++ dinner.app/src/lib/routes.ts
@@ -2,8 +2,10 @@ import SignUp from "../users/SignUp.svelte";
 import SignIn from "../users/SignIn.svelte";
 import Unauthorized from "../users/Unauthorized.svelte";
 import NotFound from "../components/NotFound.svelte";
+import MealIndex from "../meals/Index.svelte";
 
 export const authRoutes = {
+	"/meals": MealIndex,
 	"/signin": SignIn,
 	"/signup": SignUp,
 	"*": NotFound,
---
+++ dinner.app/src/meals/Index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import SignOut from "../users/SignOut.svelte";
+</script>
+
+<p>Meals index.</p>
+
+<SignOut />
