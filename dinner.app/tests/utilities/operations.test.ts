c37a0906583e5dc49511bc57f9a18166151235a8
Steven Quella <stevenquella@gmail.com>
2022-02-19 12:56:10 -0500

    Adding better expectations to validation tests. Using consistent naming.

---
+++ dinner.app/tests/utilities/operations.test.ts
@@ -0,0 +1,19 @@
+// TODO - get query store
+
+import { describe } from "vitest";
+
+describe("operations - get query store", () => {});
+
+// TODO - query, on error
+
+// TODO - query, on result
+
+// TODO - get command store
+
+// TODO - comamnd, on error
+
+// TODO - command, on result
+
+// TODO - single, on null
+
+// TODO - single, on multiple
