5b67321c61a0ab86c39c31fc268ba88b4f8fa78b
Steven Quella <stevenquella@gmail.com>
2022-02-27 11:52:35 -0500

    Validator.

--- dinner.web/src/providers/plans.ts
+++ dinner.web/src/providers/plans.ts
@@ -1,4 +1,5 @@
 import { command } from "$utilities/operations";
+import { RuleFor, Validator } from "$utilities/validations";
 import type { CommandStore } from "$utilities/_types";
 import { client, getUser } from "./_index";
 import type { Plan, PlanEdit } from "./_types";
@@ -14,9 +15,17 @@ const _plan_select = `
     notes
 `;
 
+const _id_validator = new Validator<{ id: string }>(
+	new RuleFor("id").uuid("Id must be a unique identifier.")
+);
+const _plan_validator = new Validator<PlanEdit>(
+	new RuleFor<PlanEdit>("date").required("Date is required.")
+);
+
 export async function upsertPlan(store: CommandStore, id: string, plan: PlanEdit) {
 	return await command(store, async () => {
-		// TODO - validate
+		_id_validator.ensureValid({ id });
+		_plan_validator.ensureValid(plan);
 
 		// get user
 		const user = getUser();
