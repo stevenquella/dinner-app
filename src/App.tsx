c31eadfc37fe1722485e89632d4cef739f418581
Steven Quella <stevenquella@gmail.com>
2022-12-16 14:29:11 -0500

    Create react app with clean up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Hello, world.
+        </p>
+      </header>
+    </div>
+  );
+}
