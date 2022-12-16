260252cc507e93d327d7eaa86db6d3566c356924
Steven Quella <stevenquella@gmail.com>
2022-12-16 16:56:14 -0500

    Basic authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,30 @@ import { supabase } from "./providers/client";
 
 export default function App() {
   const [session, setSession] = useState<Session | null>();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const refreshSession = async () => {
     const latestSession = await supabase.auth.getSession();
     setSession(latestSession.data.session);
+  };
+
+  const onLogin: React.FormEventHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    console.debug(response);
+    setSession(response.data.session);
+  };
+
+  const onLogout: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    await supabase.auth.signOut();
+    setSession(null);
   };
 
   useEffect(() => {
@@ -21,9 +41,41 @@ export default function App() {
   return (
     <div>
       {session != null ? (
-        <div>You are logged in.</div>
+        <div>
+          <div>You are logged in.</div>
+          <div>{session.user.id}</div>
+          <button type="button" onClick={onLogout}>
+            LOG OUT
+          </button>
+        </div>
       ) : (
-        <div>You are NOT logged in.</div>
+        <div>
+          <div>You are NOT logged in.</div>
+          <form onSubmit={onLogin}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button type="submit">LOG IN</button>
+          </form>
+        </div>
       )}
     </div>
   );
