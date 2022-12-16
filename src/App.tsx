c982528f483baf6345ad6db30390a23e62f82e76
Steven Quella <stevenquella@gmail.com>
2022-12-16 16:35:59 -0500

    Env fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import { Session } from "@supabase/supabase-js";
+import React, { useEffect, useState } from "react";
+import { supabase } from "./providers/client";
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>();
+
+  const refreshSession = async () => {
+    const latestSession = await supabase.auth.getSession();
+    setSession(latestSession.data.session);
+  };
+
+  useEffect(() => {
+    refreshSession();
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <div>
-      <header>
-        <p>
-          Hello, world.
-        </p>
-      </header>
+      {session != null ? (
+        <div>You are logged in.</div>
+      ) : (
+        <div>You are NOT logged in.</div>
+      )}
     </div>
   );
 }
